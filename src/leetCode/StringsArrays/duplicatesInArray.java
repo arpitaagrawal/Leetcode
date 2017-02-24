31458e124d006503f30988201081705270c3e9ea
arpitaagrawal <arpitaag@usc.edu>
2017-02-23 18:38:32 -0800

    Decode code using Dynamic Programing

--- src/leetCode/StringsArrays/duplicatesInArray.java
+++ src/leetCode/StringsArrays/duplicatesInArray.java
@@ -5,6 +5,7 @@ public class duplicatesInArray {
 	public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
 
 		for (int gap = 1; gap <= k; gap++) {
+
 			for (int index = 0; index <= nums.length - 1 - gap; index++) {
 				long diff1 = Math.abs(Long.valueOf(nums[index]) - Long.valueOf(nums[index+gap]));;
 				if (diff1 <= t) {
